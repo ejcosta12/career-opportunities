b4e91b4f5596b2c1b12bea70218fe32b9c67a374
Eduardo José de Paula Costa <eduardo_costa0206@hotmail.com>
2020-07-13 11:02:47 -0300

    Organizando Opportunities pode local

--- back-end/src/modules/opportunities/services/ListOpportunitiesLocationsService.ts
+++ back-end/src/modules/opportunities/services/ListOpportunitiesLocationsService.ts
@@ -2,13 +2,26 @@ import { getRepository } from 'typeorm';
 
 import Opportunities from '@modules/opportunities/infra/typeorm/entities/Opportunities';
 
+interface newOpportunities {
+  local: string;
+  opportunities: Opportunities[];
+}
+
 class ListOpportunitiesLocationsService {
-  public async execute(): Promise<Opportunities[]> {
+  public async execute(): Promise<newOpportunities[]> {
     const opportunitiesRepository = getRepository(Opportunities);
 
     const listOpportunities = await opportunitiesRepository.find();
+    const locations = listOpportunities.map(({ local }) => local)
+      .filter((local, index, locals) => (
+        locals.indexOf(local) === index
+      ));
+    const newListOpportunities = locations.map((local) => ({
+      local,
+      opportunities: listOpportunities.filter((value) => value.local === local),
+    }));
 
-    return listOpportunities;
+    return newListOpportunities;
   }
 }
 
